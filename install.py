ca78c434236891d8b160d111b2937810c5a22d02
ElDjeee <djermouni.contact@gmail.com>
2023-03-02 13:51:49 +0100

    Modif du install.py, suppression du .sh

--- install.py
+++ install.py
@@ -4,76 +4,73 @@ from jinja2 import Environment, FileSystemLoader
 # MODIFIER CES 2 LISTES POUR RENDRE LES DIFFÉRENTES PAGES
 # SI UNE SEULE PAGE NE 'COMPILE' PAS, LE PROGRAMME NE FAIT PAS LES AUTRES
 
-# Déclaration de la liste des pages à rendre
-liste_pages_a_rendre = [
-    "about",
-    #"account", #problème de compilation avec cette page
-    "admin-search-account",
-    "contact",
-    "deconnexion",
-    "editprofil",
-    "index",
-    "privacy",
-    "terms",
-]
+if len(sys.argv) == 2 and sys.argv[1] == "backend":
+    # Déclaration de la liste des pages à rendre
+    liste_pages_a_rendre = [
+        "about",
+        "account",
+        "admin-search-account",
+        "contact",
+        "deconnexion",
+        "editprofil",
+        "index",
+        "privacy",
+        "terms",
+    ]
 
-# Déclaration de la liste des pages à copier simplement
-liste_pages_a_copier = [
-    "admin-account",
-    "login_signup",
-]
+    # Déclaration de la liste des pages à copier simplement
+    liste_pages_a_copier = [
+        "admin-account",
+        "login_signup",
+    ]
 
 
-# Crée un environnement Jinja2 avec le dossier des templates
-env = Environment(loader=FileSystemLoader('.'))
+    # Crée un environnement Jinja2 avec le dossier des templates
+    env = Environment(loader=FileSystemLoader('.'))
 
-# (Re-)Création du dossier build du site
-if os.path.exists('build'):
-    shutil.rmtree('build')
-os.makedirs('build')
+    # (Re-)Création du dossier build du site
+    if os.path.exists('build'):
+        shutil.rmtree('build')
+    os.makedirs('build')
 
-# Copie du dossier backend dans le dossier build
-shutil.copytree('backend', './build/backend')
+    # Copie du dossier backend dans le dossier build
+    shutil.copytree('backend', './build/backend')
 
-# Exécution du fichier install_db.py
-exec(open("./build/backend/install_db/install_db.py").read())
+    print("Installation de la DB...")
+    # Exécution du fichier install_db.py
+    exec(open("./build/backend/install_db/install_db.py").read())
+else:
+    # Copie des fichiers sources du frontend
+    shutil.copytree('./frontend/static', './build/static')
 
-# Copie des fichiers sources du frontend
-os.makedirs('./build/frontend')
-shutil.copytree('./frontend/css', './build/frontend/css')
-shutil.copytree('./frontend/fonts', './build/frontend/fonts')
-shutil.copytree('./frontend/images', './build/frontend/images')
-shutil.copytree('./frontend/js', './build/frontend/js')
-shutil.copytree('./frontend/scss', './build/frontend/scss')
+    os.chdir('./frontend')
 
-os.chdir('./frontend')
+    # Effectue le rendu de chaque page
+    for page in liste_pages_a_rendre:
+        page = page + '.html'
+        print("Rendu de la page '" + page + "'...")
 
-# Effectue le rendu de chaque page
-for page in liste_pages_a_rendre:
-    page = page + '.html'
-    print("Rendu de la page '" + page + "'...")
+        page_path = page
+        new_page_path = "../build/" + page
 
-    page_path = page
-    new_page_path = "../build/frontend/" + page
+        # Charge le template "'page'.html"
+        template = env.get_template(page_path)
 
-    # Charge le template "'page'.html"
-    template = env.get_template(page_path)
+        # Render le template avec les paramètres souhaités
+        output = template.render()
 
-    # Render le template avec les paramètres souhaités
-    output = template.render()
+        # Enregiste le résultat dans un fichier 'page'.html
+        fichier = open(new_page_path, "a")
+        fichier.write(output)
+        fichier.close()
 
-    # Enregiste le résultat dans un fichier 'page'.html
-    fichier = open(new_page_path, "a")
-    fichier.write(output)
-    fichier.close()
+    # Copie des pages qui n'ont pas besoin de rendu
+    for page in liste_pages_a_copier:
+        page = page + '.html'
+        print("Copie de la page '" + page + "'...")
 
-# Copie des pages qui n'ont pas besoin de rendu
-for page in liste_pages_a_copier:
-    page = page + '.html'
-    print("Copie de la page '" + page + "'...")
+        page_path = page
+        new_page_path = "../build/" + page
 
-    page_path = page
-    new_page_path = "../build/frontend/" + page
-
-    shutil.copy(page_path, new_page_path)
-    
+        shutil.copy(page_path, new_page_path)
+        
