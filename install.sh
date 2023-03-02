2867f66d5c758df614245f499a959451a036beff
BLACHERE Nicolas <nicolas.blachere6@etu.univ-lorraine.fr>
2023-03-02 08:54:05 +0100

    Ajout d'un meilleur install.sh (sans supprimer l'autre)

--- install.sh
+++ install.sh
@@ -21,10 +21,11 @@ cd frontend
 
 python3.11 render_contact.py > ../build/frontend/contact.html
 python3.11 render_deconnexion.py > ../build/frontend/deconnexion.html
-python3.11 render_editprofil.py > ../build/frontend/ModifierProfil.html
+python3.11 render_editprofil.py > ../build/frontend/editprofil.html
 python3.11 render_privacy.py > ../build/frontend/privacy.html
-cp login_signup.html ../build/frontend/login_signup.html
 python3.11 render_terms.py > ../build/frontend/terms.html
 python3.11 render_account.py > ../build/frontend/account.html
 python3.11 render_index.py > ../build/frontend/index.html
 python3.11 render_about.py > ../build/frontend/about.html
+
+cp login_signup.html ../build/frontend/login_signup.html
