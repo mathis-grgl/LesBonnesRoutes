64f9bb61748f9e3794c21d209c198e4f1b959e81
Boubix88 <86961375+Boubix88@users.noreply.github.com>
2023-03-03 23:10:42 +0100

    Trajets s'affichent

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,20 +1,42 @@
 
-
 fetch('/trajets')
-  .then(response => {
-    if (!response.ok) {
-      throw new Error("Erreur lors de l'appel à la fonction get_trajets: " + response.statusText);
-    }
-    return response.json();
-  })
+  .then(response => response.json())
   .then(data => {
-    // Gérer les données de réponse ici
-    console.log(data);
+    const trajets = data;
+    const trajetsContainer = document.getElementById('row');
+    trajets.forEach(async trajet => {
+      const coordsDep = await getCoordinates(trajet.villeDepart);
+      const coordsArr = await getCoordinates(trajet.villeArrivee);
+      const trajetElement = document.createElement('div');
+      trajetElement.classList.add('col-md-6', 'col-lg-4');
+      trajetElement.innerHTML = `
+        <a href="#" class="room">
+          <figure class="img-wrap">
+            <img src="https://maps.geoapify.com/v1/staticmap?style=osm-carto&width=480&height=480&center=lonlat:${coordsDep.longitude},${coordsDep.latitude}&zoom=8.468&marker=lonlat:-${coordsArr.longitude},${coordsArr.latitude};color:%23ff0000;size:medium&apiKey=28ed3d4ce3664398aa6e2f080d227bbc" alt="Free website template" class="img-fluid mb-3">
+          </figure>
+          <div class="p-3 text-center room-info">
+            <h2>${trajet.villeDepart} - ${trajet.villeArrivee}</h2>
+            <span class="text-uppercase letter-spacing-1">${trajet.prix}€ / par trajet</span>
+          </div>
+        </a>
+      `;
+      trajetsContainer.appendChild(trajetElement);
+    });
   })
   .catch(error => {
-    // Gérer les erreurs ici
-    console.log(error.message);
+    console.error('Une erreur est survenue :', error);
   });
+
+async function getCoordinates(city) {
+  const response = await fetch(`https://nominatim.openstreetmap.org/search?q=${city}&format=json&addressdetails=1&limit=1&polygon_svg=1`);
+  const data = await response.json();
+  const latitude = data[0].lat;
+  const longitude = data[0].lon;
+  console.log("Coordonnées : " + latitude + "  " + longitude);
+  return { latitude, longitude };
+}
+ 
+//<img src="https://www.openstreetmap.org/export/staticmap?bbox=${coordsDep.latitude},${coordsDep.longitude},${coordsArr.latitude},${coordsArr.longitude}&width=480&height=320&layer=mapnik&markers=color:blue|label:A|${coordsDep.latitude},${coordsDep.longitude}&markers=color:green|label:B|${coordsArr.latitude},${coordsArr.longitude}&path=color:0x0000ff|weight:5|${coordsDep.latitude},${coordsDep.longitude};${coordsArr.latitude},${coordsArr.longitude}" alt="Free website template" class="img-fluid mb-3">
 
 
 function showSection(event) {
