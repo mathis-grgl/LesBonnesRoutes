9971773fdfbf49629269e55531da4ae77c917bde
nicola169u <romain.nicolas7@etu.univ-lorraine.fr>
2023-03-09 00:16:15 +0100

    bon jee pense que c'est presque fini mais je ne peux pas test donc je verrais ça demain matin, maitenant au lit

--- frontend/static/js/participants.js
+++ frontend/static/js/participants.js
@@ -1,69 +1,306 @@
-// Function to display a user card
-function displayUserCard(user, container) {
-    const card = document.createElement('div');
-    card.classList.add('card');
 
-    const cardBody = document.createElement('div');
-    cardBody.classList.add('card-body');
 
-    const cardTitle = document.createElement('h5');
-    cardTitle.classList.add('card-title');
-    cardTitle.textContent = user.name;
+// /getDemandesTrajet/<string:token>/<int:idTrajet>
 
-    const cardText = document.createElement('p');
-    cardText.classList.add('card-text');
-    cardText.textContent = user.email;
+// token de l'user connecté
+let token = getCookieToken();
+const params = new URLSearchParams(window.location.search);
 
-    cardBody.appendChild(cardTitle);
-    cardBody.appendChild(cardText);
-    card.appendChild(cardBody);
+// id du trajet sélectionné 
+let id = parseInt(params.get('id'));
+console.log(typeof id + ' : ' + id);
 
-    container.appendChild(card);
-}
+let urlDemande = 'trajet/getDemandesTrajet/' + token + '/' + id;
+$(document).ready(function () {
+    fetch(urlDemande)
+        .then(reponse => {
+            if (!reponse.ok) {
+                throw new Error("Network issue");
+            }
+            return reponse.json();
+        })
+        .then(data => {
+            console.log(data);
 
-// Function to display the participants
-function displayParticipants(participants) {
-    const container = document.querySelector('#participants-container');
+            let container = $("#demandes-container");
+            // Remove previous participants
+            container.empty();
+            if (typeof data !== 'undefined') {
 
-    // Remove previous participants
-    container.innerHTML = '';
 
-    // Display new participants
-    participants.forEach(participant => {
-        displayUserCard(participant, container);
+
+
+                for (let i = 0; i < data.length; i++) {
+                    displayUserDemande(data, container);
+                    // let card = $("<div>").addClass("card").data("participant", data);
+
+                    // let cardBody = $("<div>").addClass("card-body");
+
+                    // let cardTitle = $("<h5>").addClass("card-title").text(data[i].nomCompte + ' ' + data[i].prenomCompte);
+
+                    // let cardText = $("<p>").addClass("card-text").text('Cet utilisateur veut ' + data[i].nbPlaces);
+
+                    // let cardNote = $("<p>").addClass("card-text").text('Cet utilisateur a une note de ' + data[i].noteCompte + ' étoiles.');
+
+                    // let acceptBtn = $("<button>")
+                    //     .addClass("btn btn-success btn-valid")
+                    //     .html('<i class="fa fa-check"></i>');
+
+                    // let rejectBtn = $("<button>")
+                    //     .addClass("btn btn-danger btn-refuse")
+                    //     .html('<i class="fa fa-times"></i>');
+
+                    // // let removeBtn = $("<button>")
+                    // //     .addClass("btn btn-warning btn-remove")
+                    // //     .html('<i class="fa fa-trash"></i>');
+
+                    // cardBody.append(cardTitle, cardText,cardNote, acceptBtn, rejectBtn);
+                    // card.append(cardBody);
+
+                    // container.append(card);
+
+                }
+            } else {
+                container.append($("<h5>").addClass("card-title").text("Il n'y a pas de demandes."));
+
+            }
+
+        })
+        .catch(error => {
+            console.error(error);
+        });
+
+    // faire un autre fetch quand juliette l'aura fait pour afficher les utilisateurs qui participent à ce trajet
+
+
+    // Display the participants and participation requests
+    // displayParticipants(participants);
+    // displayDemandes(demandes);
+
+
+    // /acceptInTrajet/<string:token>/<int:idCompte>/<int:idTrajet>/<int:nbPlaces>/<string:accept>
+
+
+    // Add event listeners for the participation request buttons
+    $(document).on("click", ".btn-valid", function () {
+        // Handle accept button click
+        let participant = $(this).closest(".card").data("participant");
+        console.log(`On accepte le participant dont l'id est : ${participant.idCompte}`);
+
+        let nbPlaces = participant.nbPlaces;
+        console.log('Le nombre de places : ' + nbPlaces);
+
+
+        let url = 'trajet/acceptInTrajet/' + token + '/' + participant + '/' + id + '/' + nbPlaces + '/oui';
+
+        console.log(url);
+
+        fetch(url)
+            .then(reponse => {
+                if (!reponse.ok) {
+                    throw new Error('network issue');
+                }
+                return reponse.json();
+            })
+            .then(data => {
+                // code qui dit qu'on l'a accepté donc maintenant il se retrouve dans la liste des personnes participant au trajet
+                // donc maybe refaire un fetch sur les demandes et passagers et display en fonction
+            })
+            .catch(error => {
+                console.error(error);
+            });
+
     });
-}
 
-// Function to display the participation requests
-function displayDemandes(demandes) {
-    const container = document.querySelector('#demandes-container');
+    $(document).on("click", ".btn-refuse", function () {
+        // Handle reject button click
+        let participant = $(this).closest(".card").data("participant");
+        console.log(`On refuse le participant dont l'id est : ${participant.idCompte}`);
 
-    // Remove previous participation requests
-    container.innerHTML = '';
+        let nbPlaces = participant.nbPlaces;
+        console.log('Le nombre de places : ' + nbPlaces);
 
-    // Display new participation requests
-    demandes.forEach(demande => {
-        displayUserCard(demande, container);
+
+        let url = 'trajet/acceptInTrajet/' + token + '/' + participant + '/' + id + '/' + nbPlaces + '/non';
+
+        console.log(url);
+
+        fetch(url)
+            .then(reponse => {
+                if (!reponse.ok) {
+                    throw new Error('network issue');
+                }
+                return reponse.json();
+            })
+            .then(data => {
+                // code qui dit qu'on l'a accepté donc maintenant il se retrouve dans la liste des personnes participant au trajet
+                // donc maybe refaire un fetch sur les demandes et passagers et display en fonction
+            })
+            .catch(error => {
+                console.error(error);
+            });
     });
+
+
+
+    // 
+    // Add event listener for the remove participant button
+    $(document).on("click", ".btn-remove", function () {
+        // Handle remove participant button click
+        const participant = $(this).closest(".card").data("participant");
+        console.log(`On vire le passager dont l'id est : ${participant.idCompte}`);
+        console.log("Voici son prénom : " + participant.nomCompte + ' ' + participant.prenomCompte);
+    });
+});
+
+
+function displayUserDemande(data, container) {
+    let card = $("<div>").addClass("card").data("participant", data);
+
+    let cardBody = $("<div>").addClass("card-body");
+
+    let cardTitle = $("<h5>").addClass("card-title").text(data.nomCompte + ' ' + prenomCompte);
+
+    let cardText = $("<p>").addClass("card-text").text('Cet utilisateur veut ' + nbPlaces);
+
+    let cardNote = $("<p>").addClass("card-text").text('Cet utilisateur a une note de ' + noteCompte + ' étoiles.');
+
+    let acceptBtn = $("<button>")
+        .addClass("btn btn-success btn-valid")
+        .html('<i class="fa fa-check"></i>');
+
+    let rejectBtn = $("<button>")
+        .addClass("btn btn-danger btn-refuse")
+        .html('<i class="fa fa-times"></i>');
+
+    // let removeBtn = $("<button>")
+    //     .addClass("btn btn-warning btn-remove")
+    //     .html('<i class="fa fa-trash"></i>');
+
+    cardBody.append(cardTitle, cardText, cardNote, acceptBtn, rejectBtn);
+    card.append(cardBody);
+
+    container.append(card);
+
 }
+
+function displayParticipants(data, container) {
+    let card = $("<div>").addClass("card").data("participant", data);
+
+    let cardBody = $("<div>").addClass("card-body");
+
+    let cardTitle = $("<h5>").addClass("card-title").text(data.nomCompte + ' ' + prenomCompte);
+
+    let cardText = $("<p>").addClass("card-text").text('Cet utilisateur a pris ' + nbPlaces);
+
+    let cardNote = $("<p>").addClass("card-text").text('Cet utilisateur a une note de ' + noteCompte + ' étoiles.');
+
+
+
+    let removeBtn = $("<button>")
+        .addClass("btn btn-warning btn-remove")
+        .html('<i class="fa fa-trash"></i>');
+
+    cardBody.append(cardTitle, cardText, cardNote, removeBtn);
+    card.append(cardBody);
+
+    container.append(card);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Function to display the participants
+// function displayParticipants(participants) {
+//     const container = $("#participants-container");
+
+//     // Remove previous participants
+//     container.empty();
+
+//     // Display new participants
+//     participants.forEach(participant => {
+//         displayUserCard(participant, container);
+//     });
+// }
+
+// // Function to display the participation requests
+// function displayDemandes(demandes) {
+//     const container = $("#demandes-container");
+
+//     // Remove previous participation requests
+//     container.empty();
+
+//     // Display new participation requests
+//     demandes.forEach(demande => {
+//         displayUserCard(demande, container);
+//     });
+// }
 
 // Example data for testing
-const participants = [
-    { name: 'John Doe', email: 'john.doe@example.com' },
-    { name: 'Jane Doe', email: 'jane.doe@example.com' }
-];
+// const participants = [
+//     { name: 'John Doe', email: 'john.doe@example.com' },
+//     { name: 'Jane Doe', email: 'jane.doe@example.com' },
+//     { name: 'Jane Doe', email: 'jane.doe@example.com' },
+//     { name: 'Jane Doe', email: 'jane.doe@example.com' }
+// ];
 
-const demandes = [
-    { name: 'Bob Smith', email: 'bob.smith@example.com' },
-    { name: 'Alice Jones', email: 'alice.jones@example.com' }
-];
+// const demandes = [
+//     { name: 'Bob Smith', email: 'bob.smith@example.com' },
+//     { name: 'Alice Jones', email: 'alice.jones@example.com' }
+// ];
 
 // Display the participants and participation requests
 //   displayParticipants(participants);
 //   displayDemandes(demandes);
 
-function displayAll() {
-    displayParticipants(participants);
-    displayDemandes(demandes);
+// function displayAll() {
+//     displayParticipants(participants);
+//     displayDemandes(demandes);
 
-}
+// }
+
+
+
+// Function to display a user card
+// function displayUserCard(user, container) {
+//     const card = $("<div>").addClass("card").data("participant", user);
+
+//     const cardBody = $("<div>").addClass("card-body");
+
+//     const cardTitle = $("<h5>").addClass("card-title").text(user.name);
+
+//     const cardText = $("<p>").addClass("card-text").text(user.email);
+
+//     const acceptBtn = $("<button>")
+//         .addClass("btn btn-success btn-valid")
+//         .html('<i class="fa fa-check"></i>');
+
+//     const rejectBtn = $("<button>")
+//         .addClass("btn btn-danger btn-refuse")
+//         .html('<i class="fa fa-times"></i>');
+
+//     const removeBtn = $("<button>")
+//         .addClass("btn btn-warning btn-remove")
+//         .html('<i class="fa fa-trash"></i>');
+
+//     cardBody.append(cardTitle, cardText, acceptBtn, rejectBtn, removeBtn);
+//     card.append(cardBody);
+
+//     container.append(card);
+// }
