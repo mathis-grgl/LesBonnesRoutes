d23ff6f0d5616e8a1fedf28824c2e83b3618e653
nicola169u <romain.nicolas7@etu.univ-lorraine.fr>
2023-03-15 11:41:49 +0100

    vérification des formulaires pour créer et modifier trajet ok

--- frontend/static/js/creer_trajet.js
+++ frontend/static/js/creer_trajet.js
@@ -69,6 +69,8 @@ else {
 $('#forminput').submit(function (event) {
     event.preventDefault();
 
+    let valide = true;
+
     let date = $('#date-depart').val();
     console.log(date);
     let vd = $('#city_start').val();
@@ -86,9 +88,24 @@ $('#forminput').submit(function (event) {
     let precision = $('#precision').val();
     console.log(precision);
 
+    if (nbPlaces < 0) {
+        valide = false;
+        $('#nb-places').css('border', '2px solid red');
+        $('#2').empty();
+        $('#2').append($('<b>').text("Vous devez rentrer un nombre de places positif."));
+
+    }
+
+    if (prix < 0) {
+        valide = false;
+        $('#prix-place').css('border', '2px solid red');
+        $('#3').empty();
+        $('#3').append($('<b>').text("Vous devez rentrer un prix positif."));
+    }
+
 
     let dateParts = date.split('-');
-    let jour = dateParts[0];
+    let jour = dateParts[2];
     let annee = dateParts[2];
     let mois = dateParts[1] - 1;
     //let dateFormatted = dateParts[2] + '/' + dateParts[1] + '/' + dateParts[0];
@@ -96,38 +113,67 @@ $('#forminput').submit(function (event) {
     let dateObject = moment(date, 'YYYY/MM/DD');
     let dateFormatted = dateObject.format('D MMMM, YYYY');
 
+    let today = new Date();
+    let tjour = today.getDate();
+    let diff = jour - tjour;
+    console.log('voici la diff : ' + diff);
+    
+
+
+    // let diff = date2.getTime() - date1.getTime();
+
+    // Convertir la différence en heures
+    
+
+    // Vérifier si la différence est supérieure à 24 heures
+    if (diff > 1) {
+        console.log('Les deux dates sont espacées d\'au moins 24 heures');
+    } else {
+        valide = false; 
+        console.log('Les deux dates sont espacées de moins de 24 heures');
+        $('#date-depart').css('border', '2px solid red');
+        $('#1').empty();
+        $('#1').append($('<b>').text("La date doit être dans plus de 24h."));
+    }
+
+
 
     console.log('test : ' + dateFormatted);
 
-    fetch(createTrajetURL, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            'heureDepart': heure,
-            'dateDepart': dateFormatted,
-            'nbPlaces': nbPlaces,
-            'prix': prix,
-            'commentaires': commentaires,
-            'precisionRdv': precision,
-            'villeDepart': vd,
-            'villeArrivee': va
+    if (valide) {
+
+        fetch(createTrajetURL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                'heureDepart': heure,
+                'dateDepart': dateFormatted,
+                'nbPlaces': nbPlaces,
+                'prix': prix,
+                'commentaires': commentaires,
+                'precisionRdv': precision,
+                'villeDepart': vd,
+                'villeArrivee': va
+            })
         })
-    })
-        .then(reponse => {
-            if (reponse.ok) {
-                
-                window.location.href = '/mes_trajets';
-            } else {
-                alert("Probleme dans le fetch");
+            .then(reponse => {
+                if (reponse.ok) {
 
-            }
+                    window.location.href = '/mes_trajets';
+                } else {
+                    alert("Probleme dans le fetch");
 
-        }).catch(error => {
-            console.error(error);
-        });
+                }
 
+            }).catch(error => {
+                console.error(error);
+            });
+
+    }else{
+        alert('Certains champs sont mal remplis dans le formulaire.');
+    }
 
 
 
