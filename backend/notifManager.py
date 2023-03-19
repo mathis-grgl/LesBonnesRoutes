e2098eb06bdc161d74909af39fce5389d6e8b9e4
JulietteSCHILLING <juliette.schilling2@etu.univ-lorraine.fr>
2023-03-19 09:31:17 +0100

    ajout sendNotifTrajetPassagers + modif notifGroupe
    pour envoyer la notif à tous les passagers + j'ai bougé le trigger pour 1 boucle for ça sera + simple pour gérer les notifs au front

--- backend/notifManager.py
+++ backend/notifManager.py
@@ -25,6 +25,33 @@ def sendNotifTrajet(idCompteEnv, idCompteRcv, idTrajet, message):
 
 
 
+def sendNotifTrajetPassagers(idCompteEnv, idTrajet, message):
+    conn = sqlite3.connect(URI_DATABASE)
+    c = conn.cursor()
+
+    # On insère dans NOTIFICATION
+    c.execute("INSERT INTO NOTIFICATION(idCompteEnvoyeur, messageNotification) VALUES (?, ?)", (idCompteEnv, message))
+    conn.commit()  # Obligatoire pour les clés étrangères
+
+    # On recupere l'id de cette notification
+    c.execute("SELECT last_insert_rowid() FROM NOTIFICATION")
+    idNotif = c.fetchone()[0]
+
+    c.execute("INSERT INTO NOTIF_TRAJET VALUES (?, ?)", (idNotif, idTrajet))
+
+    # On insère dans NOTIF_TRAJET pour chaque passager
+    c.execute("SELECT idCompte FROM TRAJET_EN_COURS_PASSAGER WHERE idTrajet=?", (idTrajet,))
+    passagers = c.fetchall()
+    for p in passagers:
+        # On insère dans NOTIF_RECUE
+        c.execute("INSERT INTO NOTIF_RECUE VALUES (?, ?)", (p[0], idNotif))
+
+    conn.commit()
+    conn.close()
+
+
+
+
 def sendNotifGroupe(idCompteEnv, idGroupe, message):
     conn = sqlite3.connect(URI_DATABASE)
     c = conn.cursor()
@@ -40,7 +67,12 @@ def sendNotifGroupe(idCompteEnv, idGroupe, message):
     #On insère dans NOTIF_TRAJET
     c.execute("INSERT INTO NOTIF_GROUPE VALUES (?, ?)", (idNotif, idGroupe))
 
-    #On insère dans NOTIF_RECUE (grâce à un trigger)
+    # On insère dans NOTIF_GROUPE pour chaque membre du groupe
+    c.execute("SELECT idCompte FROM AMI_GROUPE WHERE idGroupe=?", (idGroupe,))
+    membres = c.fetchall()
+    for m in membres:
+        # On insère dans NOTIF_RECUE
+        c.execute("INSERT INTO NOTIF_RECUE VALUES (?, ?)", (m[0], idNotif))
 
     conn.commit()
     conn.close()
@@ -50,4 +82,5 @@ def sendNotifGroupe(idCompteEnv, idGroupe, message):
 
 
 #sendNotifTrajet(1, 2, 1, "message pour le trajet ...")
-#sendNotifGroupe(1, 1, "message pour le groupe !")
+sendNotifGroupe(1, 1, "message pour le groupe !")
+#sendNotifTrajetPassagers(1, 2, "le trajet a été modifié")
