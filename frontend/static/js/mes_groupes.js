afc044acf411f66c5ba02accfa616b21c1bb0529
nicola169u <romain.nicolas7@etu.univ-lorraine.fr>
2023-03-19 13:00:08 +0100

    affichage des groupes faits, maintenant je fais créer groupe d'amis puis après je m'occupe des actions des boutons

---
+++ frontend/static/js/mes_groupes.js
@@ -0,0 +1,62 @@
+const token = getCookieToken();
+const url = '/ami/getGroupes/' + token;
+console.log(url);
+
+function charger_groupes() {
+    console.log("test");
+    fetch(url)
+        .then(reponse => {
+            if (!reponse.ok) {
+                throw new Error("network wasnt ok");
+            }
+
+            return reponse.json();
+        })
+        .then(data => {
+            console.log(data);
+            let container = $("#groupes-container");
+            // Remove previous participants
+            container.empty();
+            for (let i = 0; i < data.length ; i++){
+                displayGroupes(data[i], container);
+            }
+
+
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}
+
+
+function displayGroupes(data, container) {
+    let card = $("<div>").addClass("card").data("groupe", data);
+
+    let cardBody = $("<div>").addClass("card-body");
+
+    let cardTitle = $("<h5>").addClass("card-title").text(data.nomGroupe);
+
+    let cardText = $("<p>").addClass("card-text").text('Ce groupe contient ' + data.nbPersonnes + ' personnes.');
+
+
+    let cardDiv = $("<div>").addClass("group-actions");
+    let acceptBtn = $("<button>")
+        .addClass("add-members")
+        .html('<i class="fas fa-user-plus"></i>');
+
+    let rejectBtn = $("<button>")
+        .addClass("edit-group")
+        .html('<i class="fas fa-edit"></i>');
+
+    let removeBtn = $("<button>")
+        .addClass("delete-group")
+        .html('<i class="fas fa-trash"></i>');
+
+    cardDiv.append(acceptBtn, rejectBtn, removeBtn);
+
+    cardBody.append(cardTitle, cardText, cardDiv);
+    card.append(cardBody);
+
+    container.append(card);
+
+}
